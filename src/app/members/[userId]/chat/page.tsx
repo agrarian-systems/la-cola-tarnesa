25e070a91ea21fed99c8e398b84a492946897598
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2024-10-11 11:26:49 +0200

    End of section 4

---
+++ src/app/members/[userId]/chat/page.tsx
@@ -0,0 +1,13 @@
+import { CardHeader, Divider, CardBody } from '@nextui-org/react';
+
+export default function ChatPage() {
+  return (
+    <>
+      <CardHeader className='text-éxl font-semibold text-secondary'>
+        Chat
+      </CardHeader>
+      <Divider />
+      <CardBody>Chat go here</CardBody>
+    </>
+  );
+}
---
+++ src/app/members/[userId]/loading.tsx
@@ -0,0 +1,13 @@
+import { Spinner } from '@nextui-org/react';
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center vertical-center'>
+      <Spinner
+        label='Chargement en cours...'
+        color='secondary'
+        labelColor='secondary'
+      />
+    </div>
+  );
+}
